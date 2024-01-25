c540ba6e303785d7bce05091a5aa7daddfef6ffe
jeff <jeff@infograb.net>
2024-01-25 23:32:03 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+setuptools.setup(
+    name="dox",
+    version="0.0.1",
+    license="MIT",
+    author="kaonmir",
+    author_email="sonjeff@naver.com",
+    description="Make all DevOps experience better",
+    long_description=open("README.md").read(),
+    url="https://github.com/kaonmir/dox",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # install_requires=open("requirements.txt").read().splitlines(),
+    install_requires=["click", "pytest"],
+    entry_points={"console_scripts": ["dox = dox.cli:cli"]},
+)
